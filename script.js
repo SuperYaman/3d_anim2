0670e827db63ce763dc68b95b4e6cca0a401e495
SuperYaman <aimane.gaillet@gmail.com>
2021-06-28 16:54:57 +0200

    third version

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const toggleButton = document.getElementById('toggle');
+const sideBar = document.getElementById('side-bar');
+
+toggleButton.addEventListener('click', show);
+
+function show() {
+    sideBar.classList.toggle('active');
+};
+
+const contain = document.querySelector('.expo');
+
+contain.addEventListener('click', () => {
+    sideBar.classList.remove('active');
+})
+
 const cube = document.querySelector('.cube');
 const container = document.querySelector('.cube-container')
 
@@ -21,7 +36,11 @@ document.body.addEventListener('click', function(e) {
 
     // CHOISIT FACE ALEATOIRE
     const classes = ['front', 'back', 'top', 'bottom', 'right', 'left'];
-    classToUse = classes[Math.floor(Match.random() * classes.length)];
+    classToUse = classes[Math.floor(Math.random() * classes.length)];
+
+    if (cube.classList.contains(classToUse)) {
+        classToUse = classes[Math.floor(Math.random() * classes.length)];
+    };
 
     // AJOUT CLASSE A MONTRER A FACE ALEATOIRE
     if (e.target.classList.contains('arrow')) {
@@ -29,5 +48,13 @@ document.body.addEventListener('click', function(e) {
         cube.classList.add(classToUse);
         cube.style.animation = 'none';
         cube.style.transition = '1.2s ease';
-    }
-})
+    };
+});
+
+
+document.querySelectorAll('.arrow').forEach(item => {
+    item.addEventListener('mouseleave', () => {
+        cube.style.animation = 'spin 1500s infinite linear';
+        cube.classList.remove('front', 'back', 'top', 'bottom', 'right', 'left');
+    });
+});
